6593fa0b729d986d06eac3e41a974ab108f93a32
Diego Cristancho <dj.ligo2002@gmail.com>
2023-10-23 10:46:51 -0500

    act arch

---
+++ src/Vista/modulos/homePage.php
@@ -0,0 +1,59 @@
+  <!-- Hero section -->
+  <section class="hero-section">
+      <div class="container">
+          <div class="row align-items-center">
+              <div class="col-md-6">
+                  <h1>Welcome to our website</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam vel tincidunt bibendum, velit sapien tincidunt nunc, vel tincidunt sapien velit vel velit. </p>
+                  <a href="#" class="btn btn-primary">Learn more</a>
+              </div>
+              <div class="col-md-6">
+                  <img src="https://via.placeholder.com/500x500" alt="Hero image" class="img-fluid">
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <!-- Call-to-action section -->
+  <section class="cta-section">
+      <div class="container">
+          <div class="row align-items-center">
+              <div class="col-md-6">
+                  <h2>Ready to get started?</h2>
+              </div>
+              <div class="col-md-6">
+                  <a href="#" class="btn btn-primary">Sign up now</a>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.min.js"></script>
+
+  <section id="locate">
+
+      <div>
+          <h2>Soporte </h2>
+          <p>Este espacio esta diseñado para que nos dejes tu inquietudes sobre errores en la apliacion.</p>
+          <div class="btn-group">
+              <button class="btn-filled-dark"><span class="material-symbols-outlined">
+                      <i class='bx bx-mail-send' style='color:#ffffff'></i>
+                  </span>Email</button>
+              <button class="btn-outline-dark btn-hover-color"><span class="material-symbols-outlined">
+                      <i class='bx bx-phone-call'></i>
+                  </span> Contact Us</button>
+          </div>
+      </div>
+  </section>
+  <footer>
+      <ul>
+          Our Services
+          <li><a href="#">Grooming</a></li>
+          <li><a href="#">Boarding</a></li>
+      </ul>
+      <ul>
+          Our Company
+          <li><a href="#">Locations &amp; Hours</a></li>
+          <li><a href="#">About Us</a></li>
+      </ul>
+  </footer>
--- src/Vista/plantilla.php
+++ src/Vista/plantilla.php
@@ -4,7 +4,9 @@ session_start();
 include_once("src/Vista/modulos/cabecera.php");
 
 include_once("src/Vista/modulos/navbar.php");
-include_once("src\Vista\modulos\perfilUsuario\misFuentes.php");
+// include_once("src\Vista\modulos\perfilUsuario/contenidoMetasdeAhorro.php");
+// include_once("src\Vista\modulos\perfilUsuario/misFuentes.php");
+include_once("src\Vista\modulos/homePage.php");
 
 
 if (isset($_SESSION["ruta"])) { 
