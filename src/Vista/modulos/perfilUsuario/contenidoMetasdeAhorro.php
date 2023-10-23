c4bd64662d926c9b29803fa1bba18d8d12858ce9
Diego Cristancho <dj.ligo2002@gmail.com>
2023-10-23 09:56:27 -0500

    se agrega el archivo contenidoMetasDeAhorro.php

---
+++ src/Vista/modulos/perfilUsuario/contenidoMetasdeAhorro.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <h2>Metas de Ahorro</h2>
+    <button>
+        <span>INGRESAR METAS</span>
+    </button>
+</div>
+
+<div class="container">
+    <img class="img" src="lic.jpg" alt="Your Image">
+    <p class="fuente">Nombre de Usuario</p>
+
+
+    <table class="">
+        <tr>
+            <th>Monto actual</th>
+            <th>Objetivo</th>
+        </tr>
+        <tr>
+            <td>$530.32</td>
+            <td></td>
+        </tr>
+    </table>
+</div>
