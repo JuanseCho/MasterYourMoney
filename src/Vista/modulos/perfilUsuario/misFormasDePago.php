a373d0f6b2d629c55619c55892e8348fee287a4c
davidsnbr <davidsnbr@gmail.com>
2023-11-02 11:29:03 -0500

    Actualización de archivos

--- src/Vista/modulos/perfilUsuario/misFormasDePago.php
+++ src/Vista/modulos/perfilUsuario/misFormasDePago.php
@@ -29,7 +29,8 @@
                 <table>
                     <thead>
                         <tr>
-                            <th> Nombre </th>
+                            <th>Nombre</th>
+                            <th>Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -50,4 +51,53 @@
             </script>
         </div>
     </div>
+</div>
+
+<!--Formulario para ingresar nuevas formas de pago-->
+
+<div class="col-md-6 col-lg-4 col-sm-10 jsDiv form-emerg" id="ventana_del_formulario_Presupuestos" style="">
+<button class="cssbuttons-io-button" id="cerrar-ventana">
+        <i class="bi bi-x-lg"></i>
+    </button>
+    <h3 class="titulos">Agregar Forma de pago</h3>
+    <form id="form_Agregar_Presupuesto">
+        <div>
+            <label for="" class="form-label">Nombre</label>
+            <select class="form-select" name="" id="select_tipoGasto" required>
+            </select>
+            <div class="valid-feedback">correcto</div>
+            <div class="invalid-feedback">error rellena el campo</div>
+        </div>
+        <button class="btn" id="Btn_new_presupuesto">Agregar</button>
+    </form>
+</div>
+
+<!--Formulario para Editar formas de pago-->
+
+<div class="col-md-3 jsDiv form-emerg" id="ventana_del_formulario_TG_Edit" style="padding: 2vh; display: non">
+<button class="cssbuttons-io-button">
+        <i class="bi bi-x-lg"></i>
+    </button>
+    <h3 class="titulos">Editar Forma de pago</h3>
+    <div class="row">
+        <div class="col-md-12 col-sm-10">
+            <form id="form_Editar_tipoDeGastos" class="needs-validation">
+                <div>
+                    <label for="" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" name="" id="txt_edit_NombreTipoGasto" placeholder="Nombre" maxlength="50" required>
+
+
+                    <div class="valid-feedback">correcto</div>
+                    <div class="invalid-feedback">error rellena el campo</div>
+
+                </div>
+
+                <button class="btn" id="btn_Edit_tipo_gasto_f" idTipoGastof="">Editar <i class="bi bi-plus-circle"></i></button>
+
+                <button class="btn" id="btn_Cancelar_edit_tipo_gasto">Cancelar <i class="bi bi-plus-circle"></i></button>
+
+            </form>
+        </div>
+    </div>
+
 </div>
