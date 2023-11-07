82aa8b28416c121706f3e57bfbefce0044eb2335
JuanseCho <120533293+JuanseCho@users.noreply.github.com>
2023-11-07 11:16:20 -0500

    bd

--- personalfinance .sql
+++ personalfinance .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-11-2023 a las 14:45:47
+-- Tiempo de generación: 07-11-2023 a las 17:13:27
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -31,7 +31,25 @@ CREATE TABLE `ahorros` (
   `idAhorro` int(11) NOT NULL,
   `Fecha` date DEFAULT NULL,
   `Descripcion` text DEFAULT NULL,
-  `Monto` decimal(10,2) DEFAULT NULL,
+  `Monto` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `ahorros`
+--
+
+INSERT INTO `ahorros` (`idAhorro`, `Fecha`, `Descripcion`, `Monto`) VALUES
+(1, '2023-11-07', 'Ahorro para vacaciones', 1000.00),
+(2, '2023-11-10', 'Ahorro para emergencias', 500.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ahorros_has_capital`
+--
+
+CREATE TABLE `ahorros_has_capital` (
+  `ahorros_idAhorro` int(11) NOT NULL,
   `capital_idCapital` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -43,10 +61,31 @@ CREATE TABLE `ahorros` (
 
 CREATE TABLE `capital` (
   `idCapital` int(11) NOT NULL,
-  `MontoInicial` int(11) DEFAULT NULL,
+  `Montoactual` int(11) DEFAULT NULL,
   `descipcion` varchar(45) DEFAULT NULL,
-  `idUsuario` int(11) NOT NULL,
-  `formapago_idFormaPago` int(11) NOT NULL
+  `formapago_idFormaPago` int(11) NOT NULL,
+  `usuarios_idUsuario` int(11) NOT NULL,
+  `fecha` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `capital`
+--
+
+INSERT INTO `capital` (`idCapital`, `Montoactual`, `descipcion`, `formapago_idFormaPago`, `usuarios_idUsuario`, `fecha`) VALUES
+(1, 1000, 'Fondo de inversión', 1, 1, '2023-11-07'),
+(2, 500, 'Cuenta bancaria', 2, 2, '2023-11-07'),
+(3, 7000000, 'sueldo ', 1, 1, '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `capital_has_presupuestos`
+--
+
+CREATE TABLE `capital_has_presupuestos` (
+  `capital_idCapital` int(11) NOT NULL,
+  `presupuestos_idPresupuesto` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -60,6 +99,14 @@ CREATE TABLE `formapago` (
   `NombreFormaPago` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `formapago`
+--
+
+INSERT INTO `formapago` (`idFormaPago`, `NombreFormaPago`) VALUES
+(1, 'Efectivo'),
+(2, 'Tarjeta de crédito');
+
 -- --------------------------------------------------------
 
 --
@@ -72,8 +119,15 @@ CREATE TABLE `gastos` (
   `Descripcion` text DEFAULT NULL,
   `Monto` int(11) DEFAULT NULL,
   `idPresupuesto` int(11) NOT NULL,
-  `idFormaPago` int(11) NOT NULL
+  `formapago_idFormaPago` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `gastos`
+--
+
+INSERT INTO `gastos` (`idGasto`, `Fecha`, `Descripcion`, `Monto`, `idPresupuesto`, `formapago_idFormaPago`) VALUES
+(1, '2023-11-15', 'Comida rápida', 20, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -84,9 +138,16 @@ CREATE TABLE `gastos` (
 CREATE TABLE `presupuestos` (
   `idPresupuesto` int(11) NOT NULL,
   `ValorAsignado` int(11) DEFAULT NULL,
-  `capital_idCapital` int(11) NOT NULL,
-  `idTipoPresupuesto` int(11) NOT NULL
+  `tipopresupuesto_idTipoPresupuesto` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `presupuestos`
+--
+
+INSERT INTO `presupuestos` (`idPresupuesto`, `ValorAsignado`, `tipopresupuesto_idTipoPresupuesto`) VALUES
+(1, 500, 1),
+(2, 300, 2);
 
 -- --------------------------------------------------------
 
@@ -98,6 +159,14 @@ CREATE TABLE `tipopresupuesto` (
   `idTipoPresupuesto` int(11) NOT NULL,
   `NombreTipoPresupuesto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tipopresupuesto`
+--
+
+INSERT INTO `tipopresupuesto` (`idTipoPresupuesto`, `NombreTipoPresupuesto`) VALUES
+(1, 'Alimentos'),
+(2, 'Transporte');
 
 -- --------------------------------------------------------
 
@@ -114,6 +183,14 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuario`, `Nombre`, `Apellido`, `Email`, `Telefono`) VALUES
+(1, 'John', 'Doe', 'john.doe@example.com', '123-456-7890'),
+(2, 'Jane', 'Smith', 'jane.smith@example.com', '987-654-3210');
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -121,16 +198,31 @@ CREATE TABLE `usuarios` (
 -- Indices de la tabla `ahorros`
 --
 ALTER TABLE `ahorros`
-  ADD PRIMARY KEY (`idAhorro`),
-  ADD KEY `fk_ahorros_capital1_idx` (`capital_idCapital`);
+  ADD PRIMARY KEY (`idAhorro`);
+
+--
+-- Indices de la tabla `ahorros_has_capital`
+--
+ALTER TABLE `ahorros_has_capital`
+  ADD PRIMARY KEY (`ahorros_idAhorro`,`capital_idCapital`),
+  ADD KEY `fk_ahorros_has_capital_capital1_idx` (`capital_idCapital`),
+  ADD KEY `fk_ahorros_has_capital_ahorros1_idx` (`ahorros_idAhorro`);
 
 --
 -- Indices de la tabla `capital`
 --
 ALTER TABLE `capital`
   ADD PRIMARY KEY (`idCapital`),
-  ADD KEY `fk_capital_usuarios_idx` (`idUsuario`),
-  ADD KEY `fk_capital_formapago1_idx` (`formapago_idFormaPago`);
+  ADD KEY `fk_capital_formapago1_idx` (`formapago_idFormaPago`),
+  ADD KEY `fk_capital_usuarios1_idx` (`usuarios_idUsuario`);
+
+--
+-- Indices de la tabla `capital_has_presupuestos`
+--
+ALTER TABLE `capital_has_presupuestos`
+  ADD PRIMARY KEY (`capital_idCapital`,`presupuestos_idPresupuesto`),
+  ADD KEY `fk_capital_has_presupuestos_presupuestos1_idx` (`presupuestos_idPresupuesto`),
+  ADD KEY `fk_capital_has_presupuestos_capital1_idx` (`capital_idCapital`);
 
 --
 -- Indices de la tabla `formapago`
@@ -144,15 +236,14 @@ ALTER TABLE `formapago`
 ALTER TABLE `gastos`
   ADD PRIMARY KEY (`idGasto`),
   ADD KEY `fk_gastos_presupuestos1_idx` (`idPresupuesto`),
-  ADD KEY `fk_gastos_formapago1_idx` (`idFormaPago`);
+  ADD KEY `fk_gastos_formapago1_idx` (`formapago_idFormaPago`);
 
 --
 -- Indices de la tabla `presupuestos`
 --
 ALTER TABLE `presupuestos`
   ADD PRIMARY KEY (`idPresupuesto`),
-  ADD KEY `fk_presupuestos_capital1_idx` (`capital_idCapital`),
-  ADD KEY `fk_presupuestos_tipopresupuesto1_idx` (`idTipoPresupuesto`);
+  ADD KEY `fk_presupuestos_tipopresupuesto1_idx` (`tipopresupuesto_idTipoPresupuesto`);
 
 --
 -- Indices de la tabla `tipopresupuesto`
@@ -174,74 +265,81 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `ahorros`
 --
 ALTER TABLE `ahorros`
-  MODIFY `idAhorro` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idAhorro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `capital`
 --
 ALTER TABLE `capital`
-  MODIFY `idCapital` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCapital` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `formapago`
 --
 ALTER TABLE `formapago`
-  MODIFY `idFormaPago` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idFormaPago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `gastos`
 --
 ALTER TABLE `gastos`
-  MODIFY `idGasto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idGasto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `presupuestos`
 --
 ALTER TABLE `presupuestos`
-  MODIFY `idPresupuesto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idPresupuesto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `tipopresupuesto`
 --
 ALTER TABLE `tipopresupuesto`
-  MODIFY `idTipoPresupuesto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idTipoPresupuesto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `ahorros`
+-- Filtros para la tabla `ahorros_has_capital`
 --
-ALTER TABLE `ahorros`
-  ADD CONSTRAINT `fk_ahorros_capital1` FOREIGN KEY (`capital_idCapital`) REFERENCES `capital` (`idCapital`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `ahorros_has_capital`
+  ADD CONSTRAINT `fk_ahorros_has_capital_ahorros1` FOREIGN KEY (`ahorros_idAhorro`) REFERENCES `ahorros` (`idAhorro`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ahorros_has_capital_capital1` FOREIGN KEY (`capital_idCapital`) REFERENCES `capital` (`idCapital`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `capital`
 --
 ALTER TABLE `capital`
   ADD CONSTRAINT `fk_capital_formapago1` FOREIGN KEY (`formapago_idFormaPago`) REFERENCES `formapago` (`idFormaPago`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_capital_usuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_capital_usuarios1` FOREIGN KEY (`usuarios_idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `capital_has_presupuestos`
+--
+ALTER TABLE `capital_has_presupuestos`
+  ADD CONSTRAINT `fk_capital_has_presupuestos_capital1` FOREIGN KEY (`capital_idCapital`) REFERENCES `capital` (`idCapital`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_capital_has_presupuestos_presupuestos1` FOREIGN KEY (`presupuestos_idPresupuesto`) REFERENCES `presupuestos` (`idPresupuesto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `gastos`
 --
 ALTER TABLE `gastos`
-  ADD CONSTRAINT `fk_gastos_formapago1` FOREIGN KEY (`idFormaPago`) REFERENCES `formapago` (`idFormaPago`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_gastos_formapago1` FOREIGN KEY (`formapago_idFormaPago`) REFERENCES `formapago` (`idFormaPago`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_gastos_presupuestos1` FOREIGN KEY (`idPresupuesto`) REFERENCES `presupuestos` (`idPresupuesto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `presupuestos`
 --
 ALTER TABLE `presupuestos`
-  ADD CONSTRAINT `fk_presupuestos_capital1` FOREIGN KEY (`capital_idCapital`) REFERENCES `capital` (`idCapital`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_presupuestos_tipopresupuesto1` FOREIGN KEY (`idTipoPresupuesto`) REFERENCES `tipopresupuesto` (`idTipoPresupuesto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_presupuestos_tipopresupuesto1` FOREIGN KEY (`tipopresupuesto_idTipoPresupuesto`) REFERENCES `tipopresupuesto` (`idTipoPresupuesto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
